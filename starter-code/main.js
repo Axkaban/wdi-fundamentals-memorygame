31119d6b25a9d5ea6c48c60a5707511b1a75f028
Axel Kaban <AK@Axels-MacBook-Pro.local>
2017-01-26 08:31:32 -0500

    commented out the div(card)s, and through js gave an id to the board and created card div in js

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,12 +1,24 @@
 console.log("JS file is connected to HTML! Woo!")
-var cardOne="queen";
-var cardTwo="king";
-var cardThree="queen";
-var cardFour="king";
+var cardOneToFour=["queen","king","queen","king"]
 
-if(cardOne === cardThree && cardTwo===cardFour){
+/*if(cardOne === cardThree && cardTwo===cardFour){
 	window.alert("You found a match!");
 }
 else{
 	window.alert("Sorry, try again.");
-}
+}*/
+var board=document.querySelector('div');
+board.id='game-board';
+var createCards=function(){
+	var gameBoard=document.getElementById('game-board');
+	for (var i=0;i<cardOneToFour.length;i++){
+	var newCard=document.createElement('div');
+	newCard.className='card';
+	gameBoard.appendChild(newCard);
+  	};
+
+};
+createCards();
+
+
+
