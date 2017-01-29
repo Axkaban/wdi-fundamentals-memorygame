90821d7185ad2fab7abd514f27749d99f6f136ff
Axel Kaban <AK@Axels-MacBook-Pro.local>
2017-01-29 00:56:48 -0500

    finished the js part adding set attributes and event listeners to createBoard, as well as the isMatch function and the isTwoCards function, with images, a cards in play array and reloading

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,24 +1,44 @@
 console.log("JS file is connected to HTML! Woo!")
-var cardOneToFour=["queen","king","queen","king"]
+var cards=['queen','king','queen','king'];
+var cardsInPlay=[];
+var gameBoard=document.getElementById('game-board');
+var createBoard=function(){
+	for(var i=0; i< cards.length; i++){
+		var newCard=document.createElement('div');
+	newCard.className='card';
+	gameBoard.appendChild(newCard);
+	gameBoard.appendChild(newCard);
+		newCard.setAttribute('data-card', cards[i]);
+		newCard.addEventListener('click', isTwoCards);
 
-/*if(cardOne === cardThree && cardTwo===cardFour){
+	};
+}
+var isTwoCards=function(){
+	cardsInPlay.push(this.getAttribute('data-card'));
+	console.log(this.getAttribute('data-card'));
+	if(this.getAttribute('data-card')==='king'){
+		this.innerHTML= "<img src='/Users/AK/fundamentals/crown-1480134_1920.jpg'>";
+	} else {
+		this.innerHTML="<img src='/Users/AK/fundamentals/crown-1647397_1280.png'>";
+	};
+	if(cardsInPlay.length===2){
+		isMatch(cardsInPlay);
+		cardsInPlay=[];
+	};
+}
+var isMatch=function(){
+	if(cardsInPlay[0]===cardsInPlay[1]){
 	window.alert("You found a match!");
+	location.reload();
 }
 else{
 	window.alert("Sorry, try again.");
-}*/
-var board=document.querySelector('div');
-board.id='game-board';
-var createCards=function(){
-	var gameBoard=document.getElementById('game-board');
-	for (var i=0;i<cardOneToFour.length;i++){
-	var newCard=document.createElement('div');
-	newCard.className='card';
-	gameBoard.appendChild(newCard);
-  	};
-
+	location.reload();
 };
-createCards();
+}
+createBoard();
+
+
 
 
 
